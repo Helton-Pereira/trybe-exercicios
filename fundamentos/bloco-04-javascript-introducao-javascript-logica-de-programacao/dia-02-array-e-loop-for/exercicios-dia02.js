2e40d913a7dbb601d4e7e3e4620befba909dafb1
Helton Pereira <helton.bfsm@gmail.com>
2022-05-14 12:54:20 -0300

    exercicios 6 e 7 finalizados

--- fundamentos/bloco-04-javascript-introducao-javascript-logica-de-programacao/dia-02-array-e-loop-for/exercicios-dia02.js
+++ fundamentos/bloco-04-javascript-introducao-javascript-logica-de-programacao/dia-02-array-e-loop-for/exercicios-dia02.js
@@ -3,6 +3,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // exercicio 1
 // console.log(numbers);
 
+
 // exercicio 2
 let soma = 0;
 
@@ -11,8 +12,10 @@ for (let index = 0; index < numbers.length; index+=1) {
 }
 // console.log(soma)
 
+
 // exercicio 3
 // console.log(soma / numbers.length)
+
 
 // exercicio 4
 if (soma / numbers.length > 20) {
@@ -20,6 +23,7 @@ if (soma / numbers.length > 20) {
 } else {
     // console.log('valor menor ou igual a 20')
 }
+
 
 // exercicio 5
 
@@ -32,5 +36,36 @@ for (let index2 = 0; index2 < numbers.length; index2+=1) {
 }
 // console.log(maiorValor)
 
+
 // exercicio 6
+
+let impar = 0;
+
+for (let index3 = 0; index3 < numbers.length; index3+=1) {
+    if (numbers[index3] % 2 != 0) {
+        impar += 1;
+     }
+    
+}
+
+// console.log(impar)
+
+
+// exercicio 7
+// pesquisar array[0]
+
+let menorNumero = numbers[0]
+
+for (let index4 = 0; index4 < numbers.length; index4+=1) {
+    if (numbers[index4] < menorNumero) {
+        menorNumero = numbers[index4];
+    }
+    
+}
+// console.log(menorNumero);
+
+
+// exercicio 8
+
+
 
