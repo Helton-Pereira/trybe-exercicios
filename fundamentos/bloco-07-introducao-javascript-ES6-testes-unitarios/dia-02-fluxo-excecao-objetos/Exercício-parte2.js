176525b4877078ac589afabddc7a26e5211801cd
Helton Pereira <helton.bfsm@gmail.com>
2022-06-10 13:32:02 -0300

    Exercício-parte2.js criado

--- fundamentos/bloco-07-introducao-javascript-ES6-testes-unitarios/dia-02-fluxo-excecao-objetos/Exercício-parte2.js
+++ fundamentos/bloco-07-introducao-javascript-ES6-testes-unitarios/dia-02-fluxo-excecao-objetos/Exercício-parte2.js
@@ -1,13 +1,52 @@
+const order = {
+  name: 'Rafael Andrade',
+  phoneNumber: '11-98763-1416',
+  address: {
+    street: 'Rua das Flores',
+    number: '389',
+    apartment: '701',
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
+      },
+      pepperoni: {
+        amount: 1,
+        price: 20,
+      }
+    },
+    drinks: {
+      coke: {
+        type: 'Coca-Cola Zero',
+        price: 10,
+        amount: 1,
+      }
+    },
+    delivery: {
+      deliveryPerson: 'Ana Silveira',
+      price: 5,
+    }
+  },
+  payment: {
+    total: 60,
+  },
+};
 
-const person = {
-    name:'Roberto',
-  };
-  
-  const lastName = {
-    lastName: 'Silva',
-  };
-  
-  const newPerson = Object.assign({},person,lastName);
-  newPerson.name = 'Gilberto';
-  console.log(newPerson);
-  console.log(person);
+const customerInfo = (order) => {
+  // Adicione abaixo as informações necessárias.
+  const address = 'address';
+
+}
+
+customerInfo(order);
+
+const orderModifier = (order) => {
+  // Adicione abaixo as informações necessárias.
+
+}
+
+orderModifier(order);
+
+// console.log(customerInfo(order));
