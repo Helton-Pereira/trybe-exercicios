bc33eb614e5417cbc531c93470742aa580fe5da8
Helton Pereira <helton.bfsm@gmail.com>
2022-06-22 19:11:04 -0300

    Exercicio 2 concluído

--- fundamentos/bloco-08-high-order-functions-javascript-ES6/exercicios.js
+++ fundamentos/bloco-08-high-order-functions-javascript-ES6/exercicios.js
@@ -1,23 +1,41 @@
-const generateEmail = (nome) => {
-    return `${nome.toLowerCase().replace(' ', '_')}@trybe.com`
-}
+// Exercício 1
 
-const createEmployee = (nome) => {
-    const employee = {
-        nomeCompleto: nome,
-        email: generateEmail(nome),
-    }
-    return employee;
-}
+// const generateEmail = (nome) => {
+//     return `${nome.toLowerCase().replace(' ', '_')}@trybe.com`
+// }
+
+// const createEmployee = (nome) => {
+//     const employee = {
+//         nomeCompleto: nome,
+//         email: generateEmail(nome),
+//     }
+//     return employee;
+// }
 
 
 
 
-const newEmployees = (callback) => {
-    const employees = {
-      id1: callback('Pedro Guerra'),
-      id2: callback('Luiza Drumond'),
-      id3: callback('Carla Paiva'),
-    }
-    return employees;
-  };
+// const newEmployees = (callback) => {
+//     const employees = {
+//       id1: callback('Pedro Guerra'),
+//       id2: callback('Luiza Drumond'),
+//       id3: callback('Carla Paiva'),
+//     }
+//     return employees;
+//   };
+
+// //// Exercício 2
+
+
+const checkNumber = (myNumber, number) => myNumber === number;
+
+const raffle = (myNumber, callback) => {
+    const number = Math.floor((Math.random() * 5) + 1);
+    if (callback(myNumber, number)) {
+        return 'Parabéns você ganhou'
+    } {
+        return 'Tente novamente'
+    };
+};
+
+console.log(raffle(2, checkNumber));
