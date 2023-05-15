37d0be7e79c9c3a96de49011dcfb1aabaa53466c
Helton-Pereira <helton.bfsm@gmail.com>
2023-05-15 14:13:05 -0300

    Exercícios dia 02

---
+++ ciencia-da-computacao/estrutura-de-dados/dia-02-hashmaps-e-Sets/exercicio.py
@@ -0,0 +1,33 @@
+def nota_partida(escolhas, adivinhas):
+    return max(escolhas.difference(adivinhas))
+
+
+def redutor(escolhas, adivinhas):
+    return min(escolhas.difference(adivinhas))
+
+
+def nota_individual(escolhas, adivinhas):
+    set_escolhas = set(escolhas)
+    set_adivinhas = set(adivinhas)
+
+    return nota_partida(set_escolhas, set_adivinhas) - redutor(
+        set_escolhas, set_adivinhas)
+
+
+def quem_ganhou(jogadas):
+    jogadores = list(jogadas.keys())
+
+    primeira_nota = nota_individual(
+        jogadas[jogadores[0]], jogadas[jogadores[1]]
+    )
+
+    segunda_nota = nota_individual(
+        jogadas[jogadores[1]], jogadas[jogadores[0]]
+    )
+
+    if primeira_nota > segunda_nota:
+        return jogadores[0]
+    elif primeira_nota < segunda_nota:
+        return jogadores[1]
+    else:
+        return None
